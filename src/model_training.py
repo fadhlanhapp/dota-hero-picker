fc1b57ee67c828263d5cdc76ff175b34d2bee114
Fadhlan Hafizh <fadhlan.papadhlan@gmail.com>
2025-08-10 16:38:48 +0700

    failed import issue

--- src/model_training.py
+++ src/model_training.py
@@ -14,7 +14,7 @@ from datetime import datetime
 from typing import Dict, List, Tuple, Optional
 import os
 import sys
-sys.path.append('..')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import Config
 
 # ML imports
